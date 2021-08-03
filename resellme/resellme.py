5e6a820ae7f18d2db29c6b3f61a2c32031a8796f
bevennyamande <bevennyamande@gmail.com>
2021-08-03 09:40:51 +0200

    Working version

--- resellme/resellme.py
+++ resellme/resellme.py
@@ -24,17 +24,17 @@ class Resellme:
         data = { "domain" : self.domain_name}
         try:
             request = requests.post(self.hostname + "/searches", data=json.dumps(data), headers = self.headers)
-            print(request.json())
+            return request.json()
         except Exception as e:
-            print(e)
+            return
 
 
-    def register_domain(self, domain_name):
+    def register_domain(self):
         """ Method to register a /domains + domain id + /register """
-        pass
-
+        print(self.domain_name)
 
 
 resellme = Resellme(api_key='eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI3IiwianRpIjoiMzQ1OGUzMThjYTg1ODA2MzZiZDkxZDYwNWY0ZTMxN2RiNWI0MmE5OTA0ZTYzZTlhYjJkZGMxYTI5MmI1MjQ5OGE3MGEyMmQ1MmE2OWMyMmYiLCJpYXQiOjE2Mjc4MjI5MjYuNjk4ODMzLCJuYmYiOjE2Mjc4MjI5MjYuNjk4ODM2LCJleHAiOjE2NTkzNTg5MjYuNjkxNjA0LCJzdWIiOiIyOTUiLCJzY29wZXMiOltdfQ.e5MH1D_ovuKxqzNtJzxnNrIJ5mBMaechItu7YfNiczAz3nR_POWDWdSxPEwgN4YgscnmnEaPgKFrxM6qVS3lNU6DeZj8-gybVsx2VccbCzoINfss-7-Q661px7hoCE1D4aNX0OnyTOmVW8rp2Ut_6E26AsMa6Si59upsQRkZFiLe6sgesl42B3Dk74p3qFeIm0uu53THgbwLIYxBXWMLQzj0g_RPKzQQCMSClicZKa-6fa6k-sWV6zkLHkuTXKZ4hOK2xE4jrLwd6EAytJehZf_JKK6qwrVIW9tWun21pwxVChxyFcrfx8SxUD9Z6-XKDXp4lJAMAU7GukYRlYXWg1itruXuxkk2dk1r8tklxuiWYmf9PKmVaUinlu12ri_0d_JwJ-uixJdcICn1hR5RuJGGG7zebllXNnKQPAOhpxSvuqUqiJVfMBJmfpTKhU1xPXLE7hCRaWMntmshEOciszYdgqjjFCUJaJ7NE51TJdAqGTaQwTTeZylfvAU5bdjCAIbYaQDrJ7tWKniC8y6UjbOSkNzDiG3Xmv7p7b8T7pKOvAdWcin5AFZacMkiaubY6_8qmR5GlDpTikLixuBVKZN7xrrBf14LpVGi588WtJTgnjcDFa1B2ZUUI2Tjarj-CwyvS6ZLzkXovIuYBDyP0xPls-gVR6q3l2gQTXANKZs')
 
-resellme.search_domain('fake.co.zw')
+print(resellme.search_domain('fake.co.zw'))
+resellme.register_domain()
